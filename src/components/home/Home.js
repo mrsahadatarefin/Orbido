558bd766bdfe3b6632a7c3acfe896dab54ad7557
mrsahadatarefin <sahadatarifin806@gmail.com>
2023-04-13 17:02:50 +0600

    app setpu

---
+++ src/components/home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mt-32">
+      <h1 className=" text-4xl lg:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#1FD8B6] to-[#C5018E]
+ text-center ">
+        make your dream website <br />
+        Stand out 
+        in a crowd
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import {Link} from 'react-scroll'
+import logo from '../../assets/logo.png'
+const NavBar = () => {
+    const [nav,setNav] = useState(false) 
+
+    const links = [
+        {
+          id: 1,
+          link: "home",
+        },
+        {
+          id: 2,
+          link: "About Us",
+        },
+        {
+          id: 3,
+          link: "Features",
+        },
+        {
+          id: 4,
+          link: "Solution",
+        },
+        
+      ];
+  return (
+    <div className="px-4 bg-black flex justify-between items-center w-full h-20 text-white static
+     top-0 ">
+      <div className="mt-5">
+        
+        <img src={logo} alt="logo" className="mt-8"/>
+      </div>
+      <ul className=" hidden md:flex">
+        {links.map(({link,id}) => (
+          <li
+            className="px-4 cursor-pointer capitalize font-medium text-gray-300 hover:scale-105 duration-200"
+            key={id} link={link}
+          >
+            
+            <Link >{link}</Link>
+          </li>
+        ))}
+      </ul>
+      <div onClick={()=>setNav(!nav)} className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden">
+       {
+        nav? <FaTimes size={30} /> : <FaBars size={30}/>
+       }
+
+       {nav && (
+    <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-300">
+   {links.map(({link,id}) => (
+    <li
+    className="px-4 cursor-pointer capitalize py-6 text-4xl"
+      key={id}
+    >
+    <Link onClick={()=> setNav(!nav)}  > {link}</Link>
+    </li>
+  ))}
+  
+  
+ 
+ </ul>
+       ) }
+      
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
